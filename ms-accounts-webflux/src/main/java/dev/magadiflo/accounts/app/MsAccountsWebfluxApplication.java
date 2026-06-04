39ee51bae8c1b153550efe4a111ab29ffeaa8a94
Martín Díaz <magadiflo@gmail.com>
2026-06-04 13:29:05 -0500

    Agrega plugins al pom.xml

---
+++ ms-accounts-webflux/src/main/java/dev/magadiflo/accounts/app/MsAccountsWebfluxApplication.java
@@ -0,0 +1,13 @@
+package dev.magadiflo.accounts.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MsAccountsWebfluxApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MsAccountsWebfluxApplication.class, args);
+    }
+
+}
---
+++ ms-accounts-webflux/src/test/java/dev/magadiflo/accounts/app/MsAccountsWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.magadiflo.accounts.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MsAccountsWebfluxApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
